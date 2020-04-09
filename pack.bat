be51b2c3a19c8546647564af1d2e49b4a0b969a7
ONEO <oneo.me@outlook.com>
2020-04-09 11:43:28 +0800

    修复条件

--- pack.bat
+++ pack.bat
@@ -1,4 +1,4 @@
 @echo off
-REM dotnet build -c Debug /p:Platform=x64
-REM dotnet build -c Debug /p:Platform=x86
+dotnet build -c Debug /p:Platform=x64
+dotnet build -c Debug /p:Platform=x86
 nuget pack -OutputDirectory Packages
